6dfd040f1eea6b36f1a9ee6737ced7a2fd10858d
KAUE <139394184+kauefs@users.noreply.github.com>
2023-11-21 13:21:30 -0300

    Sydney.py

--- Sydney.py
+++ Sydney.py
@@ -78,10 +78,12 @@ st.title('Inside Sydney Airbnb')
 st.markdown('''
 [![LinkedIn](https://img.shields.io/badge/LinkedIn-kauefs-blue.svg)](https://www.linkedin.com/in/kauefs/)
 [![GitHub](https://img.shields.io/badge/GitHub-kauefs-black.svg)](https://github.com/kauefs/)
-[!(https://img.shields.io/badge/GitHub-000000?logo=github&logoColor=white)](https://github.com/kauefs/)
+
+[![GitHub](https://img.shields.io/badge/GitHub-000000?logo=github&logoColor=white)](https://github.com/kauefs/)
 <a href='https://github.com/kauefs'           alt='GitHub'  ><img src='https://img.shields.io/badge/GitHub-000000?logo=github&logoColor=white'    />
 </a>
 <a href='https://www.linkedin.com/in/kauefs/' alt='LinkedIn'><img src='https://img.shields.io/badge/LinkedIn-0077B5?logo=linkedin&logoColor=white'/></a>
+
 [![](https://img.shields.io/badge/Python-3-blue.svg)](https://www.python.org/)
 [![GPLv3 license](https://img.shields.io/badge/License-Apache2-black.svg)](https://www.apache.org/licenses/LICENSE-2.0)
             ''')
