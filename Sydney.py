9a5abbb6fa7f52d99725920e071ea7febe6e914a
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-02 04:23:40 -0300

    Sydney.py

--- Sydney.py
+++ Sydney.py
@@ -50,43 +50,45 @@ df           = load_data()
 hood         = df.neighbourhood.unique().tolist()
 room         = df.room.unique().tolist()
 # SIDE:
-st.sidebar.success(  'SydNey AirBnB')
-st.sidebar.header(   'InSide AirBnB')
-st.sidebar.subheader('SYDNEY       ')
-st.sidebar.markdown( 'Data Analysis')
-st.sidebar.write('Data Base Date: 2023.06.06')
-# PlaceHolder for Table:
-table        = st.sidebar.empty()
-# PlaceHolder for Filtered Listings:
-SideBarInfo  = st.sidebar.empty()
-# MultiSelect for NeighBourHood:
-FilteredHood = st.sidebar.multiselect(label  = 'NeighBourHood:',
-                                      options=  hood,
-                                      default=['Sydney, New South Wales, Australia'])
-# MultiSelect for RoomType:
-FilteredRoom = st.sidebar.multiselect(label  = 'RoomType:',
-                                      options=  room,
-                                      default=['Entire home/apt','Private room','Shared room','Hotel room'])
-# Price Slider Selection:
-FilteredPrice= st.sidebar.slider('Price:', 0.01, 599.99, 255.75, step=None)
-# Filtered Data:
-FilteredDF   = df[(df.neighbourhood.isin(FilteredHood)) & (df.room.isin(FilteredRoom)) & (df.price <= FilteredPrice)]
-# Updating PlaceHoder:
-SideBarInfo.info('{} Filtered Listings'.format(FilteredDF.shape[0]))
-# MAIN:
-st.title('InSide SydNey AirBnB')
-st.markdown('''
+# st.sidebar.title('ƊⱭȾɅViƧi🧿Ƞ')
+st.sidebar.markdown('''
 [![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
 [![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
 [![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
 [![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/badge/Apache_2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
+[![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
             ''')
-with st.container():
-     cols = st.columns(3)
-     with cols[0]:st.empty()
-     with cols[1]:st.write('23 September 2023')
-     with cols[2]:st.empty()
+st.sidebar.text( '23 September 2023')
+st.sidebar.success(  'InSide AirBnB')
+st.sidebar.divider(                 )
+st.sidebar.header(   'SYDNEY       ')
+st.sidebar.subheader('Data Analysis')
+st.sidebar.markdown( '''Source: [InSide AirB**n**B](http://insideairbnb.com/get-the-data.html)''')
+st.sidebar.write('Data Base Date: 2023.06.06')
+# NeighbourHood MultiSelect:
+FilteredHood  = st.sidebar.multiselect(label  = 'NeighBourHood:',
+                                      options=  hood,
+                                      default=['Sydney, New South Wales, Australia'])
+# Room Type MultiSelect:
+FilteredRoom  = st.sidebar.multiselect(label  = 'RoomType:',
+                                      options=  room,
+                                      default=['Entire home/apt','Private room','Shared room','Hotel room'])
+# Price Slider:
+FilteredPrice = st.sidebar.slider('Price:', 0.01, 599.99, 255.75, step=None)
+# Filtered Listings PlaceHolder:
+SideBarInfo   = st.sidebar.empty()
+# Filtered Data:
+FilteredDF    = df[(df.neighbourhood.isin(FilteredHood)) & (df.room.isin(FilteredRoom)) & (df.price <= FilteredPrice)]
+# PlaceHoder UpDate:
+SideBarInfo.info('{} Filtered Listings'.format(FilteredDF.shape[0]))
+#   Table           PlaceHolder:
+table         = st.sidebar.empty()
+st.sidebar.divider(              )
+st.sidebar.markdown('''©2023™ [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/)''')
+# MAIN:
+st.divider(                    )
+st.title('Inside Sydney Airbnb')
+st.divider(                    )
 # WordCloud:
 #SYD         =  pd.read_csv('datasets/SYD20230606AirBnB.csv.gz')
 #select      =['description']
@@ -200,9 +202,4 @@ if  st.sidebar.checkbox('Simple'):
     st.subheader(       'Simple Map:')
     st.map(FilteredDF)
 st.sidebar.divider()
-with st.sidebar.container():
-     cols = st.columns(3)
-     with cols[0]:st.empty()
-     with cols[1]:st.markdown('''©2023™''')
-     with cols[2]:st.empty()
 st.toast('SYD!', icon='🌃')
