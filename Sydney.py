2bbfea65d5be495a253074ebe4a9d9c95ff75003
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 16:35:02 -0300

    Sydney.py

--- Sydney.py
+++ Sydney.py
@@ -85,7 +85,7 @@ st.sidebar.markdown('''
 [![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
 [![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
 
-2023.09.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+2023.09.23 &copy; 2023 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
 # MAIN:
 st.divider(                    )
 st.title('Inside Sydney Airbnb')
