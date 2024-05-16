60ed14ceefca9e8ec6a5aef8b6afec7af53651dc
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 16:34:10 -0300

    Sydney.py

--- Sydney.py
+++ Sydney.py
@@ -50,14 +50,7 @@ df           = load_data()
 hood         = df.neighbourhood.unique().tolist()
 room         = df.room.unique().tolist()
 # SIDE:
-st.sidebar.title(   'ƊⱭȾɅViƧi🧿Ƞ')
-st.sidebar.markdown('''
-[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
-[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
-[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
-[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
-            ''')
+st.sidebar.title(    'ƊⱭȾɅViƧi🧿Ƞ' )
 st.sidebar.success(  'InSide AirBnB')
 st.sidebar.divider(                 )
 st.sidebar.header(   'SYDNEY       ')
@@ -85,7 +78,14 @@ st.sidebar.info('{} Filtered Listings'.format(FilteredDF.shape[0]))
 #   Table           PlaceHolder:
 table         = st.sidebar.empty()
 st.sidebar.divider(              )
-st.sidebar.markdown('''2023.09.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+st.sidebar.markdown('''
+[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
+[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
+[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
+[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
+[![License]( https://img.shields.io/github/license/kauefs/StreamLit?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71&color=D22128)](https://www.apache.org/licenses/LICENSE-2.0)
+
+2023.09.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
 # MAIN:
 st.divider(                    )
 st.title('Inside Sydney Airbnb')
